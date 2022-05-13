3e578dd8f48cfa6eb3ea373737921333c9fbb3ae
zainbmaged <101899558+zainbmaged@users.noreply.github.com>
2022-05-13 18:59:13 +0200

    Update test.c
    this the macros for driver functions please use same names

--- test.c
+++ test.c
@@ -1,1 +1,8 @@
 //this file is our C projet code 
+//the driver code
+
+//the macros used please use same names
+#define SET_BIT(reg,bit)     (reg|= (1<<bit))
+#define CLR_BIT(reg,bit)     (reg&=(~(1<<bit)))
+#define TOG_BIT(reg,bit)      (reg^=(1<<bit))
+#define READ_BIT(reg,bit)     (reg&(1<<bit)>>bit)
