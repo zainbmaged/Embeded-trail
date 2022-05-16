889a0f675c2cc3a77d0ca82e1130673bfe4e8d6e
Sar00-Ahmed <104655280+Sar00-Ahmed@users.noreply.github.com>
2022-05-16 14:54:02 +0200

    Update Delay.h

--- Delay.h
+++ Delay.h
@@ -1,3 +1,3 @@
 void SystemInit(void);
 void SysTic_wait(unsigned long delay);
-void genericDelay(unsigned long time); //delay in millisec
+void generic_Delay(unsigned long time); //delay in millisec
