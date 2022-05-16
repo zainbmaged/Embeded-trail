4a15547628b24da564b062f2fffd05c1cab73ed4
Sar00-Ahmed <104655280+Sar00-Ahmed@users.noreply.github.com>
2022-05-16 19:59:40 +0200

    Create drivers_headers.h

---
+++ drivers_headers.h
@@ -0,0 +1,42 @@
+//LCD_Driver
+
+static void Send_Pulse(void);
+void LCD_Vsend_CMD(char cmd);
+void LCD_Vsend_charac(char charac);
+void LCD_Vsend_String(char *data);
+void LCD_CLR_Screen();
+void LCD_Move_Cursor(char row, char col);
+void LCD_vInit(void);
+
+// switches and Leds drivers
+
+void Switch_Init();
+unsigned char readSW1();
+unsigned char readSW2();
+unsigned char readSW3();
+void Switch_Init_Pullup();
+
+void Led_Array_vInit(void);
+void Led_Array_On();
+void Led_Array_Off(void);
+void Led_Array_Toggle();
+void Blink (unsigned char blinks);
+
+//delay 
+
+void SysTic_wait(unsigned long delay);
+void generic_Delay(unsigned long time); //delay in millisec
+unsigned char Cooking_Countdown (unsigned char *cooking_Time);
+
+//DIO driver
+void port_init (unsigned char port);
+void DIO_vport_dir (unsigned char port, unsigned char pin, unsigned char dir);
+void DIO_vwrite_PIN(unsigned char port_name, unsigned char DATA ,unsigned char pin_number);
+void DIO_vtoggle_PIN(unsigned char port_name ,unsigned char pin_number);
+unsigned char DIO_u8READ_PIN(unsigned char port_name ,unsigned char pin_number);
+void DIO_setPortDir( unsigned char portName, unsigned char direction );
+void DIO_writePort( unsigned char portName, unsigned char data);
+unsigned char DIO_u8ReadPort( unsigned char portName);
+void DIO_enablePullUp( unsigned char portName, unsigned char pinNo );
+void DIO_u8WriteHighNibble ( unsigned char portName, unsigned char data);
+void DIO_u8WriteLowNibble ( unsigned char portName, unsigned char data);
