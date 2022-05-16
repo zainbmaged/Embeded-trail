3b70b26cb1e1ec34bd9fcef419098501a7dd1737
Sar00-Ahmed <104655280+Sar00-Ahmed@users.noreply.github.com>
2022-05-16 11:20:03 +0200

    Update LCD_driver.c

--- LCD_driver.c
+++ LCD_driver.c
@@ -4,6 +4,20 @@
 
 enum ctrl{E, Rs, Rw};
 
+//connected ports
+#define K_PORT_Row 'A'	//rows port
+#define K_PORT_Col 'B'	//columns port
+#define CTRL_PORT 'A'
+#define Data_PORT 'D'
+
+//commands macros
+#define CLR_Screen 0x01 //clears the LCD
+#define Turn_On 0x0C		//turn on screen only command
+#define Turn_On_Cursor 0x0E //turns on the screen and the cursor
+#define Entry_Mode 0x06 //LCD cursor moves after entering a char
+#define Eight_Bit 0x38
+
+
 
 static void Send_Pulse(void){	//pulse sent to enable
 	DIO_vwrite_PIN(CTRL_PORT,E ,1);
