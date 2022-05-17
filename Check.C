1e074413875c64af348290c7155fd805c43936cb
Sar00-Ahmed <104655280+Sar00-Ahmed@users.noreply.github.com>
2022-05-17 18:17:37 +0200

    Update Check.C

--- Check.C
+++ Check.C
@@ -1,7 +1,7 @@
 unsigned char Is_Valid	(int min, int max, int number){ 
-	if(number<min)
+	if(number<=min)
 		return 0;
-	else if(number> max)
+	else if(number>=max)
 		return 0;
 	else
 		return 1;
